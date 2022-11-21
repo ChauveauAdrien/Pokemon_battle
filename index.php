6a6899827dcbffe157d75def07a664ccea4a7a9b
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-11-21 15:04:01 +0100

    update

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 
 require_once './classes/Pokemon.php';
+include 'vendor/autoload.php';
 
-$pokemon1 = new Pokemon('Pikachu', 'elec', 300, 60, 40, 20, 10);
-$pokemon2 = new Pokemon('Dracofeu', 'feu', 200, 100, 80, 60, 40);
-
-
-
-$pokemon1->attacked($pokemon2->attack());
+$client = new GuzzleHttp\Client();
+$response = $client->request('GET', 'https://pokeapi.co/api/v2/pokemon/27');
+var_dump($response->getBody());
 
 
-print_r($pokemon1);
+// $pokemon1 = new Pokemon('Pikachu', 'elec', 300, 60, 40, 20, 10);
+// $pokemon2 = new Pokemon('Dracofeu', 'feu', 200, 100, 80, 60, 40);
+
+
+
+// $pokemon1->attacked($pokemon2->attack());
+
+
+// print_r($pokemon1);
