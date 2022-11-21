c38d5a3ffaab681b6ec3a172e2d4bab54ebb2004
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-11-21 16:14:39 +0100

    update

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 require_once __DIR__.'/includes/header.php';
 
+require_once './classes/Pokemon.php';
+include 'vendor/autoload.php';
+
+$client = new GuzzleHttp\Client();
+$res = $client->request('GET', 'https://pokeapi.co/api/v2/pokemon-habitat/');
+echo $res->getStatusCode();
+// "200"
+echo $res->getHeader('content-type')[0];
+// 'application/json; charset=utf8'
+echo $res->getBody();
+// {"type":"User"...'
+
 ?>
 <main class="main__wrapper">
     <div class="main__top">
@@ -16,17 +28,7 @@ require_once __DIR__.'/includes/header.php';
             <a class="btn__a" href="/vues/name.php">Oui</a>
         </div>
     </div>
-require_once './classes/Pokemon.php';
-include 'vendor/autoload.php';
 
-$client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'https://pokeapi.co/api/v2/pokemon-habitat/');
-echo $res->getStatusCode();
-// "200"
-echo $res->getHeader('content-type')[0];
-// 'application/json; charset=utf8'
-echo $res->getBody();
-// {"type":"User"...'
 
 </main>
 
